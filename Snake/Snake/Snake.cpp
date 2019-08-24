2cddea558445620a83e95a0bbdaf28a50c69f6f2
Max Krasnoshluk <krasnoshluk@gmail.com>
2019-08-24 23:44:33 +0300

    List point

--- Snake/Snake/Snake.cpp
+++ Snake/Snake/Snake.cpp
@@ -6,7 +6,8 @@
 #include <iostream>
 #include <conio.h>
 #include <windows.h>
-
+#include <list>     
+#include <iterator> // заголовок итераторов
 using namespace std;
 
 int main()
@@ -15,5 +16,14 @@ int main()
 	p1.draw();
 	point p2(5, 9, '@');
 	p2.draw();
+	point p3(7, 8, '*');
+	p3.draw();
+	point p4(8, 1, '!');
+	p4.draw();
+	list<point> Pointlist;
+	Pointlist.push_back(p1);
+	Pointlist.push_back(p2);
+	Pointlist.push_back(p3);
+	Pointlist.push_back(p4);
 	
 }
