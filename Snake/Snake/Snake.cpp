15404a89d4698019d3ff886ea6140d6e07623d7a
Max Krasnoshluk <krasnoshluk@gmail.com>
2019-08-25 23:51:09 +0300

    inheritance (наследование)

--- Snake/Snake/Snake.cpp
+++ Snake/Snake/Snake.cpp
@@ -2,7 +2,7 @@
 //
 
 #include "pch.h"
-#include "wall.h"
+#include "figure.h"
 #include "point.h"
 #include <iostream>
 #include <conio.h>
@@ -13,14 +13,28 @@ using namespace std;
 
 int main()
 {
-	point p1(2, 6, '&');   
+	system("mode con cols=80 lines=28");
+	
+	wallhorizont lineh_up(2, 78, 2, 'h');
+	lineh_up.drow();
+	wallhorizont lineh_dawn(2, 78, 23, 'h');
+	lineh_dawn.drow();
+	wallvertical linev_left(2, 24, 2, 'v');
+	linev_left.drow();
+	wallvertical linev_rigth(2, 24, 78, 'v');
+	linev_rigth.drow();
+	point p1(6, 6, '&');
+	p1.draw();
+	COORD position = { 0, 24 };
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleCursorPosition(hConsole, position);
+	cout << endl;
+	/*point p1(2, 6, '&');
 	p1.draw();
 	point p2(5, 9, '@');
 	p2.draw();
 	point p3(7, 8, '*');
 	p3.draw();
 	point p4(8, 1, '!');
-	p4.draw();
-	wall line(5, 10, 10, '-');
-	line.drow();
+	p4.draw();*/
 }
